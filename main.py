411a8360cdecbce926a28d1b5265660125781017
Chelsea <chelsea.mgarc@gmail.com>
2023-04-14 11:44:08 -0400

    some more api stuff

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ import folium
 
 print("hello")
 
+
 '''
 TODO: Hi guys, welcome to our repo for this project. I am making a todo list so that you guys have something to start off of.
 1. Make your own branch (git checkout -b "yourname_branch" "main"). pls make your changes on your own branch and then 
     push when you are sure of your changes.
 
+'''
+
+'''Note: I made an API key.
+key = B6Z6FYVFXXLWJT5ZIKFH
+
+but we have to do more authorization stuff to use it.
+https://www.eventbrite.com/platform/docs/authentication#authenticate-api-requests
 '''
