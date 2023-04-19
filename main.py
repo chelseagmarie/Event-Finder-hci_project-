e03dcdadfcc9f0802a94c77251e4111b54051b74
Chelsea <chelsea.mgarc@gmail.com>
2023-04-19 17:44:45 -0400

    checkbox and filtering

--- main.py
+++ main.py
@@ -85,6 +85,27 @@ def venues_setlist(city_selected):
 
     return venues_set
 
+@st.cache_data
+def genres_available():
+    genres_set = set()
+    url = f"https://api.seatgeek.com/2/genres?client_id={client_ID}"
+    request = requests.get(url).json()
+    for i in range(0,len(request["genres"])):
+        genres_set.add(request["genres"][i]["name"])
+
+    genres_list = list(genres_set)
+    genres_list.sort()
+    return genres_list
+
+@st.cache_data
+def filter_perfomers_by_genre(genre):
+    performers_set = set()
+    url = f"https://api.seatgeek.com/2/performers?client_id={client_ID}&genres[primary].slug={genre}"
+    request = requests.get(url).json()
+    for i in range(0,len(request["performers"])):
+        performers_set.add(request["performers"][i]["name"])
+    return performers_set
+
 
 # Events
 st.title("Events Near You!")
@@ -101,3 +122,81 @@ if country:
             st.subheader("List of Venues Near you!")
             st.write(venues_setlist(city))
             map_creator(26, -80.15)
+
+
+st.write("Check Genre(s) of Interest")
+check0 = st.checkbox(genres_available()[0])
+check1 = st.checkbox(genres_available()[1])
+check2 = st.checkbox(genres_available()[2])
+check3 = st.checkbox(genres_available()[3])
+check4 = st.checkbox(genres_available()[4])
+check5 = st.checkbox(genres_available()[5])
+check6 = st.checkbox(genres_available()[6])
+check7 = st.checkbox(genres_available()[7])
+check8 = st.checkbox(genres_available()[8])
+check9 = st.checkbox(genres_available()[9])
+check10 = st.checkbox(genres_available()[10])
+check11 = st.checkbox(genres_available()[11])
+check12 = st.checkbox(genres_available()[12])
+check13 = st.checkbox(genres_available()[13])
+check14 = st.checkbox(genres_available()[14])
+check15 = st.checkbox(genres_available()[15])
+check16 = st.checkbox(genres_available()[16])
+check17 = st.checkbox(genres_available()[17])
+check18 = st.checkbox(genres_available()[18])
+check19 = st.checkbox(genres_available()[19])
+check20 = st.checkbox(genres_available()[20])
+
+selected = []
+if check0:
+    selected.append(genres_available()[0])
+if check1:
+    selected.append(genres_available()[1])
+if check2:
+    selected.append(genres_available()[2])
+if check3:
+    selected.append(genres_available()[3])
+if check4:
+    selected.append(genres_available()[4])
+if check5:
+    selected.append(genres_available()[5])
+if check6:
+    selected.append(genres_available()[6])
+if check7:
+    selected.append(genres_available()[7])
+if check8:
+    selected.append(genres_available()[8])
+if check9:
+    selected.append(genres_available()[9])
+if check10:
+    selected.append(genres_available()[10])
+if check11:
+    selected.append(genres_available()[11])
+if check12:
+    selected.append(genres_available()[12])
+if check13:
+    selected.append(genres_available()[13])
+if check14:
+    selected.append(genres_available()[14])
+if check15:
+    selected.append(genres_available()[15])
+if check16:
+    selected.append(genres_available()[16])
+if check17:
+    selected.append(genres_available()[17])
+if check18:
+    selected.append(genres_available()[18])
+if check19:
+    selected.append(genres_available()[19])
+if check20:
+    selected.append(genres_available()[20])
+
+st.write("performers for this genre(s)")
+
+for genre in selected:
+    st.write(filter_perfomers_by_genre(genre))
+
+
+
+
+    
